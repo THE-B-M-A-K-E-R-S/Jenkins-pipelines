49aebb64c309471303ae0a88721326c51a98f205
Zgon <karim.mannai7@gmail.com>
2022-11-06 17:17:50 +0100

    SonarLint fixes

--- src/test/java/tn/esprit/rh/achat/services/CategorieProduitServiceImplMockTest.java
+++ src/test/java/tn/esprit/rh/achat/services/CategorieProduitServiceImplMockTest.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
-public class CategorieProduitServiceImplMock {
+class CategorieProduitServiceImplMockTest {
 
     @Autowired
     ICategorieProduitService cps;
